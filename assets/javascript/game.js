2ac61a80eeb873cea6f152d135c31c5e83e6397e
Christopher Mangin <mangin.christopher@gmail.com>
2019-10-09 16:46:23 -0400

    made the necessary changes from homework feedback

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,108 +1,100 @@
-$(document).ready(function() {
-   
-    var target = Math.floor(Math.random()*101+19);
-    
-    $("#targetnumber").text(target);
-
-        var blueCrystal = Math.floor(Math.random()*11+1);
-        var redCrystal = Math.floor(Math.random()*11+1);
-        var greenCrystal = Math.floor(Math.random()*11+1);
-        var yellowCrystal = Math.floor(Math.random()*11+1);
-
-
-        var userScore = 0;
         var wins = 0;
         var losses = 0;
-
-
-    $("#gameWins").text(wins);
-    $("#gameLosses").text(losses);
-
-
-    function reset() {
-        target = Math.floor(Math.random()*101+19);
-        userScore = 0;
-        console.log("target number: " + target);
+        var target = Math.floor(Math.random() * 101) + 19;
         $("#targetnumber").text(target);
-        
 
-        blueCrystal = Math.floor(Math.random()*11+1);
-        redCrystal = Math.floor(Math.random()*11+1);
-        greenCrystal = Math.floor(Math.random()*11+1);
-        yellowCrystal = Math.floor(Math.random()*11+1);
-    }
+        var blueCrystal = Math.floor(Math.random() * 11) + 1;
+        var redCrystal = Math.floor(Math.random() * 11) + 1;
+        var greenCrystal = Math.floor(Math.random() * 11) + 1;
+        var yellowCrystal = Math.floor(Math.random() * 11) + 1;
 
-    function winner() {
-        alert("You win!");
-        wins++;
-        console.log("Wins: " + wins);
-       $("#gameWins").text(wins);
-        reset();
-    }
+        $("#gameWins").html(wins);
+        $("#gameLosses").html(losses);
 
-    function loser() {
-        alert("You lose!");
-        losses++;
-        console.log("Losses: " + losses);
-        $("#gameLosses").text(wins);
-        reset();
-    }
+        userScore = 0;
+        $("#currentScore").text(userScore);
 
-    $("#blue").on("click", function(){
-        userScore = userScore + blueCrystal;
-        console.log("New user score: " + userScore);
-        $("#userScore").text(userScore);
-        
+        function restartGame() {
 
-        if (userScore == target) {
-            winner();
+
+            target = Math.floor(Math.random() * 101) + 19;
+            $("#targetnumber").text(target);
+            blueCrystal = Math.floor(Math.random() * 11) + 1;
+            redCrystal = Math.floor(Math.random() * 11) + 1;
+            greenCrystal = Math.floor(Math.random() * 11) + 1;
+            yellowCrystal = Math.floor(Math.random() * 11) + 1;
+
+            userScore = 0;
+            $("#currentScore").text(userScore);
+
         }
-        else if (userScore > target) {
-            loser();
+
+
+
+        function winner() {
+            alert("You win!");
+            wins++;
+            $("#wins").text(wins);
+            restartGame();
         }
-    })
 
-
-    $("#red").on("click", function(){
-        userScore = userScore + redCrystal;
-        console.log("New user score: " + userScore);
-        $("#userScore").text(userScore);
-        
-
-        if (userScore == target) {
-            winner();
+        function loser() {
+            alert("You lose!");
+            losses++;
+            $("#losses").text(losses);
+            restartGame();
         }
-        else if (userScore > target) {
-            loser();
-        }
-    })
+
+        $("#blue").on("click", function () {
+            userScore = userScore + blueCrystal;
+            console.log("New user score: " + userScore);
+            $("#currentScore").text(userScore);
 
 
-    $("#green").on("click", function(){
-        userScore = userScore + greenCrystal;
-        console.log("New user score: " + userScore);
-        $("#userScore").text(userScore);
-        
+            if (userScore == target) {
+                winner();
+            } else if (userScore > target) {
+                loser();
+            }
+        });
 
-        if (userScore == target) {
-            winner();
-        }
-        else if (userScore > target) {
-            loser();
-        }
-    })
 
-    $("#yellow").on("click", function(){
-        userScore = userScore + yellowCrystal;
-        console.log("New user score: " + userScore);
-        $("#userScore").text(userScore);
-        
+        $("#red").on("click", function () {
+            userScore = userScore + redCrystal;
+            console.log("New user score: " + userScore);
+            $("#currentScore").text(userScore);
 
-        if (userScore == target) {
-            winner();
-        }
-        else if (userScore > target) {
-            loser();
-        }
-    })
-})
+
+            if (userScore == target) {
+                winner();
+            } else if (userScore > target) {
+                loser();
+            }
+        })
+
+
+        $("#green").on("click", function () {
+            userScore = userScore + greenCrystal;
+            console.log("New user score: " + userScore);
+            $("#currentScore").text(userScore);
+
+
+            if (userScore == target) {
+                winner();
+            } else if (userScore > target) {
+                loser();
+            }
+        })
+
+        $("#yellow").on("click", function () {
+            userScore = userScore + yellowCrystal;
+            console.log("New user score: " + userScore);
+            $("#currentScore").text(userScore);
+
+
+            if (userScore == target) {
+                winner();
+            } else if (userScore > target) {
+                loser();
+            }
+        });
